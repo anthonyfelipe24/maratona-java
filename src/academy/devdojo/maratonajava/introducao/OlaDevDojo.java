6b04508c5c12f36a2046aa4c4454b6f95cbaf336
Anthony felipe <anthonyfelipe149@gmail.com>
2026-02-09 16:29:29 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um Comentário <b>javadoc</b>
+ * coisas novas
+ * muito novas
+ */
+public class OlaDevDojo {
+    // Isso é um comentário de uma LINHA
+    /*
+    Comentário de multiplas linhas
+     */
+    public static void main(String[] args)  {
+        System.out.println("KA-ME-KA-ME-HAAA");
+    }
+}
