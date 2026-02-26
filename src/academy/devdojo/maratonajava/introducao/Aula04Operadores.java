eba1927f0ba2d83a40712e7665c7b85daba9aa8a
Anthony felipe <anthonyfelipe149@gmail.com>
2026-02-26 16:10:21 -0300

    aula de operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -28,7 +28,7 @@ public class Aula04Operadores {
         int idade = 29;
         float salario = 3500F;
         boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >=4612;
-        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3382;
 
         System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
