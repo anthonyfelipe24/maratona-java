80af2439ef491427946a05eb21e22e032875d41b
Anthony felipe <anthonyfelipe149@gmail.com>
2026-06-23 20:21:22 -0300

    terminando a parte de array dimensionais.

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -1,5 +1,7 @@
 package academy.devdojo.maratonajava.introducao;
 
+import java.sql.SQLOutput;
+
 public class Aula08ArraysMultidimensionais01 {
     public static void main(String[] args) {
         // 1 Array: 1,2,3,4,5 Representa os Meses
@@ -14,10 +16,16 @@ public class Aula08ArraysMultidimensionais01 {
         dias[1][2] = 29;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length ; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
             }
         }
+        System.out.println("--------------");
 
+        for(int [] arrBase: dias){
+            for(int num: arrBase){
+                System.out.println(num);
+            }
+        }
     }
 }
