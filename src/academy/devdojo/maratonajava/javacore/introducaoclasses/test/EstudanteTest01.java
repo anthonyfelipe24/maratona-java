db0f3e69acba365ffe71381586afcdebb7ed3ccc
Anthony felipe <anthonyfelipe149@gmail.com>
2026-06-26 20:19:57 -0300

    Dando inicio a POO.

--- src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -6,7 +6,7 @@ public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
         estudante.nome = "Luffy";
-        estudante.idade = 21;
+        estudante.idade = 22;
         estudante.sexo = 'M';
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
