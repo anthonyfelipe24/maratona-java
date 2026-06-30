7a70327b8022b6463e7bf7f650d24c804d44494d
Anthony felipe <anthonyfelipe149@gmail.com>
2026-06-30 18:18:34 -0300

    exercicio de classe

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -4,16 +4,20 @@ import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
-        Carro carro = new Carro();
-        carro.nome = "Chevrolet";
-        carro.modelo = "Onix";
-        carro.ano = 2023;
-        System.out.println("Marca: "+ carro.nome + " Modelo: "+ carro.modelo + " Ano: " + carro.ano);
-
+        Carro carro1 = new Carro();
         Carro carro2=  new Carro();
-        carro2.nome = "JEEP";
+        carro1.marca = "Chevrolet";
+        carro1.modelo = "Onix";
+        carro1.ano = 2023;
+
+        carro2.marca = "JEEP";
         carro2.modelo = "Compass";
         carro2.ano = 2024;
-        System.out.println("Marca: "+ carro2.nome + " Modelo: "+ carro2.modelo + " Ano: " + carro2.ano);
+
+        carro2 = carro1;
+
+        System.out.println("Marca: "+ carro1.marca + " Modelo: "+ carro1.modelo + " Ano: " + carro1.ano);
+        System.out.println("Marca: "+ carro2.marca + " Modelo: "+ carro2.modelo + " Ano: " + carro2.ano);
+
     }
 }
