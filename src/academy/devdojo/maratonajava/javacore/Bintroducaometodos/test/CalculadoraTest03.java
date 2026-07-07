cb9cd21b33f2679d4669d3c121cba09a6ea0dfda
Anthony felipe <anthonyfelipe149@gmail.com>
2026-07-07 16:08:50 -0300

    exercicio de metodos,parametro e retorno

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,8 +5,10 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest03 {
     public static void main(String[] args) {
        Calculadora calculadora = new Calculadora();
-       double result = calculadora.divideDoisNumeros(20, 2);
-
-        System.out.println(result);
+       double result = calculadora.divideDoisNumeros(20, 0);
+       System.out.println(result);
+       System.out.println(calculadora.divideDoisNumeros02(20,0));
+       System.out.println("----------");
+       calculadora.imprimeDivisaoDeDoisNumeros(86,5);
     }
 }
